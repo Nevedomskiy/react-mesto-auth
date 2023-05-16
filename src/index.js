e5f43fd1a310d97a1efcd695ff6b3e7e64c006c3
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-05-16 14:40:30 +0400

    add Readme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,11 +6,11 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
 
 
